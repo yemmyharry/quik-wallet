ea5422cad33fff08193b303c0e03b56701b838cd
yemmyharry <yemmyharry@gmail.com>
2022-03-31 13:07:31 +0100

    Change type for time fields

--- internal/core/domain/resource/wallet.go
+++ internal/core/domain/resource/wallet.go
@@ -1,8 +1,10 @@
 package resource
 
+import "time"
+
 type Wallet struct {
-	ID        int64  `json:"id"`
-	Balance   string `json:"balance"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	ID        int64     `gorm:"primary_key;auto_increment" json:"id"`
+	Balance   string    `gorm:"type:varchar(32)" json:"balance"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
