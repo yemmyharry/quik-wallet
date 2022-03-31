f1a98d15c08d86c90e442dc0eddf69f365009344
yemmyharry <yemmyharry@gmail.com>
2022-03-31 16:31:20 +0100

    Add error log when an error occurs

--- internal/adapter/repositories/mysql/resource/repository.go
+++ internal/adapter/repositories/mysql/resource/repository.go
@@ -1,11 +1,15 @@
 package resource
 
-import "quik-wallet/internal/core/domain/resource"
+import (
+	"quik-wallet/internal/core/domain/resource"
+	"quik-wallet/internal/core/logger"
+)
 
 func (r *WalletRepositoryDB) GetBalance(id int64) (resource.Wallet, error) {
 	var balance resource.Wallet
 	err := r.db.First(&balance, id)
 	if err != nil {
+		logger.Error(err.Error)
 		return balance, err.Error
 	}
 	return balance, err.Error
