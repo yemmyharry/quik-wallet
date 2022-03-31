adaa819cce0a47487efd2094e68e6894a024cd5e
yemmyharry <yemmyharry@gmail.com>
2022-03-31 13:07:04 +0100

    Use gorm query method in get balance method

--- internal/adapter/repositories/mysql/resource/repository.go
+++ internal/adapter/repositories/mysql/resource/repository.go
@@ -2,11 +2,11 @@ package resource
 
 import "quik-wallet/internal/core/domain/resource"
 
-func (r *WalletRepositoryDB) GetBalance(userId int64) (resource.Wallet, error) {
-	var wallet resource.Wallet
-	err := r.db.QueryRow("SELECT * FROM wallets WHERE user_id = ?", userId)
+func (r *WalletRepositoryDB) GetBalance(id int64) (resource.Wallet, error) {
+	var balance resource.Wallet
+	err := r.db.First(&balance, id)
 	if err != nil {
-		return wallet, err.Err()
+		return balance, err.Error
 	}
-	return wallet, err.Err()
+	return balance, err.Error
 }
