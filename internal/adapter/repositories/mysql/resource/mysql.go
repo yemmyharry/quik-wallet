e1b3a46e92af80eb4f428bb62eb4f7507b1c68f4
yemmyharry <yemmyharry@gmail.com>
2022-03-31 13:06:30 +0100

    Use gorm and automigrate table

--- internal/adapter/repositories/mysql/resource/mysql.go
+++ internal/adapter/repositories/mysql/resource/mysql.go
@@ -1,19 +1,28 @@
 package resource
 
 import (
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"log"
+	domain "quik-wallet/internal/core/domain/resource"
 )
 
 type WalletRepositoryDB struct {
-	db *sql.DB
+	db *gorm.DB
 }
 
+const DB_USERNAME = "root"
+const DB_PASSWORD = "12345678"
+const DB_NAME = "wallet"
+const DB_HOST = "127.0.0.1"
+const DB_PORT = "3306"
+
 func NewWalletRepositoryDB() *WalletRepositoryDB {
-	client, err := sql.Open("mysql", "root:12345678@tcp(localhost:3306)/wallet")
+	dsn := DB_USERNAME + ":" + DB_PASSWORD + "@tcp" + "(" + DB_HOST + ":" + DB_PORT + ")/" + DB_NAME + "?" + "charset=utf8mb4&parseTime=True&loc=Local"
+	client, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
+	client.AutoMigrate(&domain.Wallet{})
 	return &WalletRepositoryDB{client}
 }
