62c0ef35c1c2281523ac096efd1070e3fb1165bd
yemmyharry <yemmyharry@gmail.com>
2022-03-31 16:29:32 +0100

    Add info log when server is started

--- quik-wallet.go
+++ quik-wallet.go
@@ -4,16 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 	adapter "quik-wallet/internal/adapter/api/resource"
 	"quik-wallet/internal/adapter/repositories/mysql/resource"
+	"quik-wallet/internal/core/logger"
 	services "quik-wallet/internal/core/services/resource"
 )
 
 func main() {
-	//start server using go gin framework
 	router := gin.Default()
+	//router.Use(logger.LoggerToFile())
 	database := resource.NewWalletRepositoryDB()
 	service := services.New(database)
 	handler := adapter.NewHTTPHandler(service, router)
 	handler.Routes(router)
+	logger.Info("Starting server on port 8080")
 	handler.Route.Run(":8080")
-	//log.Fatal(router.Run(":8080"))
 }
