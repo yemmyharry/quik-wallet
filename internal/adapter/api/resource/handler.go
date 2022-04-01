61231fbf191332ce28a92a270e2f4eef5ed70866
yemmyharry <yemmyharry@gmail.com>
2022-04-01 11:34:18 +0100

    Add debit wallet by id method

--- internal/adapter/api/resource/handler.go
+++ internal/adapter/api/resource/handler.go
@@ -23,7 +23,6 @@ func (s *HTTPHandler) GetWalletBalanceByID() gin.HandlerFunc {
 	}
 }
 
-//  credits money on a given wallet id
 func (s *HTTPHandler) CreditsWalletByID() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		id := c.Param("id")
@@ -40,6 +39,34 @@ func (s *HTTPHandler) CreditsWalletByID() gin.HandlerFunc {
 			return
 		}
 		wallet, err := s.walletService.Credit(int64(ID), w2.Amount)
+		if err != nil {
+			logger.Error(err)
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(200, wallet)
+	}
+}
+
+func (s *HTTPHandler) DebitsWalletByID() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		ID, _ := strconv.Atoi(id)
+		w2 := struct {
+			Amount string `json:"amount"`
+		}{}
+		err := c.ShouldBindJSON(&w2)
+		if err != nil {
+			logger.Error(err)
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		wallet, err := s.walletService.Debit(int64(ID), w2.Amount)
 		if err != nil {
 			logger.Error(err)
 			c.JSON(400, gin.H{
