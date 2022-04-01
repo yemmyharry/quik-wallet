4f32392c13075fbb177e8092f3b767729d12ac74
yemmyharry <yemmyharry@gmail.com>
2022-04-01 11:35:17 +0100

    Add debit route

--- internal/adapter/api/resource/router.go
+++ internal/adapter/api/resource/router.go
@@ -8,6 +8,6 @@ func (s *HTTPHandler) Routes(router *gin.Engine) {
 	apirouter := router.Group("api/v1")
 	apirouter.GET("/wallets/:id/balance", s.GetWalletBalanceByID())
 	apirouter.POST("/wallets/:id/credit", s.CreditsWalletByID())
-	//apirouter.POST("/wallet/debit", s.DebitWalletByID())
+	apirouter.POST("/wallets/:id/debit", s.DebitsWalletByID())
 	router.NoRoute(func(c *gin.Context) { c.JSON(404, "no route") })
 }
