e4f78c230528d5c71b2abf8746127c0f6a9c07e7
yemmyharry <yemmyharry@gmail.com>
2022-04-01 10:34:54 +0100

    Add credit wallet by id method

--- internal/adapter/api/resource/handler.go
+++ internal/adapter/api/resource/handler.go
@@ -2,6 +2,7 @@ package resource
 
 import (
 	"github.com/gin-gonic/gin"
+	"quik-wallet/internal/core/logger"
 	"strconv"
 )
 
@@ -9,15 +10,38 @@ func (s *HTTPHandler) GetWalletBalanceByID() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		id := c.Param("id")
 		ID, _ := strconv.Atoi(id)
-		//if err != nil {
-		//	c.JSON(400, gin.H{
-		//		"error": err.Error(),
-		//	})
-		//	return
-		//}
 
 		wallet, err := s.walletService.GetBalance(int64(ID))
 		if err != nil {
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(200, wallet)
+	}
+}
+
+//  credits money on a given wallet id
+func (s *HTTPHandler) CreditsWalletByID() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		ID, _ := strconv.Atoi(id)
+		w2 := struct {
+			Amount string `json:"amount"`
+		}{}
+		err := c.ShouldBindJSON(&w2)
+		if err != nil {
+			logger.Error(err)
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		wallet, err := s.walletService.Credit(int64(ID), w2.Amount)
+		if err != nil {
+			logger.Error(err)
 			c.JSON(400, gin.H{
 				"error": err.Error(),
 			})
