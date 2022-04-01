c206437fc7714a068320cde4254e7838739fd549
yemmyharry <yemmyharry@gmail.com>
2022-04-01 11:36:42 +0100

    Add debit method to service interface

--- internal/ports/resource/service.go
+++ internal/ports/resource/service.go
@@ -5,5 +5,5 @@ import domain "quik-wallet/internal/core/domain/resource"
 type WalletService interface {
 	GetBalance(id int64) (domain.Wallet, error)
 	Credit(id int64, amount string) (domain.Wallet, error)
-	//Debit(id int64, amount string) (domain.Wallet, error)
+	Debit(id int64, amount string) (domain.Wallet, error)
 }
