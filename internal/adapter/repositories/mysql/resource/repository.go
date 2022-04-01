d65f77f71b7dfc31357e2e3ee3021160e1a76a73
yemmyharry <yemmyharry@gmail.com>
2022-04-01 10:36:44 +0100

    Add credit method to repository db

--- internal/adapter/repositories/mysql/resource/repository.go
+++ internal/adapter/repositories/mysql/resource/repository.go
@@ -1,6 +1,7 @@
 package resource
 
 import (
+	"github.com/shopspring/decimal"
 	"quik-wallet/internal/core/domain/resource"
 	"quik-wallet/internal/core/logger"
 )
@@ -13,4 +14,18 @@ func (r *WalletRepositoryDB) GetBalance(id int64) (resource.Wallet, error) {
 		return balance, err.Error
 	}
 	return balance, err.Error
+}
+
+func (r *WalletRepositoryDB) Credit(id int64, amount string) (resource.Wallet, error) {
+	var wallet resource.Wallet
+	_ = r.db.Where("id = ?", id).First(&wallet)
+	aDec, _ := decimal.NewFromString(amount)
+	bDec, _ := decimal.NewFromString(wallet.Balance)
+	wallet.Balance = aDec.Add(bDec).String()
+	err := r.db.Save(&wallet)
+	if err != nil {
+		logger.Error(err.Error)
+		return wallet, err.Error
+	}
+	return wallet, nil
 }
