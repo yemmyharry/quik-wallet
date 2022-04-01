18312d7449439f01e6349575bb03834908b031ac
yemmyharry <yemmyharry@gmail.com>
2022-04-01 23:36:00 +0100

    Use env variables

--- quik-wallet.go
+++ quik-wallet.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"os"
 	adapter "quik-wallet/internal/adapter/api/resource"
 	"quik-wallet/internal/adapter/repositories/mysql/resource"
 	"quik-wallet/internal/core/logger"
@@ -9,11 +11,19 @@ import (
 )
 
 func main() {
+	err := godotenv.Load("quik-wallet.env")
+	if err != nil {
+		logger.Error("Error loading .env file")
+	}
 	router := gin.Default()
 	database := resource.NewWalletRepositoryDB()
 	service := services.New(database)
 	handler := adapter.NewHTTPHandler(service)
 	handler.Routes(router)
 	logger.Info("Starting server on port 8080")
-	router.Run(":8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	router.Run(":" + port)
 }
