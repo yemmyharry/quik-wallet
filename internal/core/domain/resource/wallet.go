216b3d06bfecdd0df6af40eb43f0fa99f77762c4
yemmyharry <yemmyharry@gmail.com>
2022-04-01 10:40:17 +0100

    Add amount to struct

--- internal/core/domain/resource/wallet.go
+++ internal/core/domain/resource/wallet.go
@@ -5,6 +5,7 @@ import "time"
 type Wallet struct {
 	ID        int64     `gorm:"primary_key;auto_increment" json:"id"`
 	Balance   string    `gorm:"type:varchar(32)" json:"balance"`
+	Amount    string    `gorm:"type:varchar(32)" json:"amount"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
