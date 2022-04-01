0c413be66a33c62a310fcda8015ab8ab9fb0ba9c
yemmyharry <yemmyharry@gmail.com>
2022-04-01 10:39:49 +0100

    Add credit method to service interface

--- internal/ports/resource/service.go
+++ internal/ports/resource/service.go
@@ -4,4 +4,6 @@ import domain "quik-wallet/internal/core/domain/resource"
 
 type WalletService interface {
 	GetBalance(id int64) (domain.Wallet, error)
+	Credit(id int64, amount string) (domain.Wallet, error)
+	//Debit(id int64, amount string) (domain.Wallet, error)
 }
