3c37de5958afeb45ef6eabcbe5ad5456e205cdc2
yemmyharry <yemmyharry@gmail.com>
2022-04-01 10:37:14 +0100

    Add credit method to repository interface

--- internal/ports/resource/repository.go
+++ internal/ports/resource/repository.go
@@ -4,4 +4,6 @@ import domain "quik-wallet/internal/core/domain/resource"
 
 type WalletRepository interface {
 	GetBalance(id int64) (domain.Wallet, error)
+	Credit(id int64, amount string) (domain.Wallet, error)
+	//Debit(id int64, amount string) (domain.Wallet, error)
 }
